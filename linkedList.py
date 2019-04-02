45cfbf7e4cdaed155ed20dfc68cf395dd6bbf9d0
Evgeniy Sova <nevilwhite@gmail.com>
2019-04-02 14:36:13 +0000

    Update linkedList.py

--- linkedList.py
+++ linkedList.py
@@ -11,7 +11,7 @@ class ListNode:
         return self.next_node
 
     def get_previous_node(self):
-        return self.next_node
+        return self.previous_node
 
     def set_previous_node(self, new_previous_node):
         self.previous_node = new_previous_node
@@ -21,6 +21,11 @@ class ListNode:
 
     def set_next_node(self, new_next_node):
         self.next_node = new_next_node
+
+
+class LinkedListSingle:
+    def __init__(self):
+        pass
 
 
 class LinkedList:
@@ -56,19 +61,41 @@ class LinkedList:
                 count_node = count_node + 1
                 current_node = current_node.get_next_node()
         else:
-            return "Linked List is empty"
-
+            count_node = 'Linked List is empty'
         return count_node
 
 
+# ==== Proof zone ====
 L = LinkedList()
-
-L.add_node_head(10001)
+L.add_node_head('Node 1')
 L.add_node_tail(44440)
 L.add_node_head(20002)
 L.add_node_tail(55550)
 L.add_node_head(30003)
 L.add_node_tail(66660)
 
-print(L.size())
-print('fin')
+
+def get_id_node(node):
+    if node.get_previous_node() is None:
+        _prev_node = 'no_object_links'
+    else:
+        _prev_node = str(id(node.get_previous_node()))
+    _current_node = str(id(node))
+    if node.get_next_node() is None:
+        _next_node = 'no_object_links'
+    else:
+        _next_node = str(id(node.get_next_node()))
+    return _prev_node, _current_node, _next_node
+
+
+current_node = L.tail_list
+print('+-------------- Proof: Objects ID list -------------------+')
+print('| Previous: \t\t| Current: \t\t\t| Next:           |')
+while current_node is not None:
+    get_id_node(current_node)
+    prev_node, current, next_node = get_id_node(current_node)
+    print('| ' + prev_node + '\t| ' + current + '\t| ' + next_node + ' |')
+    current_node = current_node.get_next_node()
+
+print('+-------------------+-------------------+-----------------+')
+print('List size: ' + str(L.size()))

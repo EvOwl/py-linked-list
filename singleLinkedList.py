3b4d38781f145c3d6bc0298a5535f5f3af2e03aa
Evgeniy Sova <nevilwhite@gmail.com>
2019-04-05 17:35:20 +0300

    Fix .size() function

--- singleLinkedList.py
+++ singleLinkedList.py
@@ -1,35 +1,59 @@
 from listNode import ListNode
 
 
-class SingleLinkedList:
+class _SingleLinkedList:
     def __init__(self):
         self._head_node = None
-        self._count_nodes = 0
 
     def get_head_node(self):
         return self._head_node
 
-    def size(self):
-        return self._count_nodes
-
     def add(self, value):
         pass
 
+    def find(self, node):
+        if node is not self._head_node:
+            current_node = self._head_node.get_next_node()
+            while current_node is not None:
+                if current_node is node:
+                    return True
+                elif current_node is self._head_node:
+                    current_node = None
+                else:
+                    current_node = current_node.get_next_node()
+        else:
+            return True
+
+    def size(self):
+        if self._head_node is not None:
+            current_node = self._head_node.get_next_node()
+            count = 1
+            while current_node is not None:
+                if current_node is None or current_node is self._head_node:
+                    current_node = None
+                else:
+                    count = count + 1
+                    current_node = current_node.get_next_node()
+        else:
+            count = 0
+        return count
+
     def delete_node(self, node):
         current_node = self._head_node
-        # AHTUNG!!!
-        # If I inject NODE in not this Linked List this is crashed this code
-        while current_node is not None:
-            tail_node = current_node.get_next_node()
-            if tail_node is node:
-                tail_node = tail_node.get_next_node()
-                self._count_nodes = self._count_nodes - 1
-                return current_node.set_next_node(tail_node)
-            else:
-                current_node = current_node.get_next_node()
+        if self.find(node):
+            while current_node is not None:
+                tail_node = current_node.get_next_node()
+                if tail_node is node:
+                    tail_node = tail_node.get_next_node()
+                    current_node.set_next_node(tail_node)
+                    return True
+                else:
+                    current_node = current_node.get_next_node()
+        else:
+            return False
 
 
-class SingleWayList(SingleLinkedList):
+class SingleWayList(_SingleLinkedList):
     def add(self, value):
         tmp_node = ListNode(value)
         if self._head_node is None:
@@ -37,10 +61,9 @@ class SingleWayList(SingleLinkedList):
         else:
             tmp_node.set_next_node(self._head_node)
             self._head_node = tmp_node
-        self._count_nodes = self._count_nodes + 1
 
 
-class SingleWayLoopedList(SingleLinkedList):
+class SingleWayLoopedList(_SingleLinkedList):
     def add(self, value):
         tmp_node = ListNode(value)
         if self._head_node is None:
@@ -50,5 +73,4 @@ class SingleWayLoopedList(SingleLinkedList):
             tmp_node.set_next_node(self._head_node.get_next_node())
             self._head_node.set_next_node(tmp_node)
             self._head_node = tmp_node
-        self._count_nodes = self._count_nodes + 1
 
